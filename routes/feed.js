eb216a84ee494171622f8b973c075501c3ddc903
Haboc, Bryan Joe <bryan.joe.haboc@accenture.com>
2022-04-06 15:28:41 +0800

    trial commit

--- routes/feed.js
+++ routes/feed.js
@@ -1,13 +1,17 @@
-const express = require('express');
+const express = require("express");
 
-const feedController = require('../controllers/feed');
+const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
+router.get("/", (req, res, next) => {
+  res.send("hello world");
+});
+
 // GET /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get("/posts", feedController.getPosts);
 
 // POST /feed/post
-router.post('/post', feedController.createPost);
+router.post("/post", feedController.createPost);
 
 module.exports = router;
